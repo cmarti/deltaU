f1a2bec16bd0a84ebb5010986079fa4324fcbc5b
Carlos Marti <carlos199m1@gmail.com>
2026-08-08 16:32:31 -0400

    code updated

--- code/simulations/calc_r2_curves.py
+++ code/simulations/calc_r2_curves.py
@@ -1,4 +1,4 @@
-from code.models2 import TruncatedModel, evaluate_predictions
+from code.models import TruncatedModel, evaluate_predictions
 
 import numpy as np
 import pandas as pd
@@ -6,58 +6,63 @@ from gpmap.inference import (
     ConnectednessModelRegression,
     LocalEpistasisRegression,
     MinimumEpistasisInterpolator,
+    SitesVCregression,
     VCregression,
 )
 
 if __name__ == "__main__":
     np.random.seed(0)
-    print("Loading data")
-    data = pd.read_csv("data/processed/simulations.csv", index_col=0)
-    X, y, y_var = data.index.values, data.y.values, data.y_var.values
+    for base_model in ["ssVC"]: #["ler", "ssVC"]:
+        print(f"Loading data fronm {base_model} model simulations...")
+        data = pd.read_csv(f"data/processed/simulations.{base_model}.csv", index_col=0)
+        X, y, y_var = data.index.values, data.y.values, data.y_var.values
 
-    models = {
-        "Additive": TruncatedModel(genotypes=X, max_k=1),
-        "Pairwise": TruncatedModel(genotypes=X, max_k=2),
-        "Threeway": TruncatedModel(genotypes=X, max_k=3),
-        "MEI": MinimumEpistasisInterpolator(
-            seq_length=8, alphabet_type="rna", P=2
-        ),
-        "VC": VCregression(seq_length=8, alphabet_type="rna"),
-        "CN": ConnectednessModelRegression(seq_length=8, alphabet_type="rna"),
-        "LER": LocalEpistasisRegression(
-            seq_length=8, alphabet_type="rna", P=2
-        ),
-    }
+        models = {
+            "Additive": TruncatedModel(genotypes=X, max_k=1),
+            "Pairwise": TruncatedModel(genotypes=X, max_k=2),
+            "Threeway": TruncatedModel(genotypes=X, max_k=3),
+            "MEI": MinimumEpistasisInterpolator(
+                seq_length=8, alphabet_type="rna", P=2
+            ),
+            "VC": VCregression(seq_length=8, alphabet_type="rna"),
+            "CN": ConnectednessModelRegression(seq_length=8, alphabet_type="rna"),
+            "LER": LocalEpistasisRegression(
+                seq_length=8, alphabet_type="rna", P=2
+            ),
+            "ssVC": SitesVCregression(seq_length=8, alphabet_type="rna"),
+        }
 
-    print("Calculating R2 curves")
-    results = []
-    for p in np.geomspace(0.01, 0.99, 10):
-        n_train = int(p * data.shape[0])
-        for _ in range(3):
-            train_idx = np.random.choice(
-                data.index, size=n_train, replace=False
-            )
-            train = data.loc[train_idx, :]
-            test_idx = ~np.isin(data.index, train_idx)
-            test = data.loc[test_idx, :]
-
-            X_train = train.index.values
-            f_train = train.f.values
-            y_train = train.y.values
-            y_var_train = train.y_var.values
-            X_test, f_test = test.index.values, test.f.values
-
-            for label, model in models.items():
-                model.fit(X=X_train, y=y_train, y_var=y_var_train)
-                y_pred = model.predict()
-
-                f_train_pred = y_pred.loc[X_train, "f"].values
-                f_test_pred = y_pred.loc[X_test, "f"].values
-
-                record = evaluate_predictions(
-                    y_pred, X_train, X_test, y_train, f_test, label=label, p=p
+        print("Calculating R2 curves")
+        results = []
+        for p in np.geomspace(0.01, 0.99, 10):
+            print(f"  Using {p*100:.1f}% of data for training...")
+            n_train = int(p * data.shape[0])
+            for _ in range(3):
+                train_idx = np.random.choice(
+                    data.index, size=n_train, replace=False
                 )
-                results.append(record)
+                train = data.loc[train_idx, :]
+                test_idx = ~np.isin(data.index, train_idx)
+                test = data.loc[test_idx, :]
 
-    results = pd.DataFrame(results)
-    results.to_csv("results/simulations.r2.csv")
+                X_train = train.index.values
+                f_train = train.f.values
+                y_train = train.y.values
+                y_var_train = train.y_var.values
+                X_test, f_test = test.index.values, test.f.values
+
+                for label, model in models.items():
+                    print(f"    Fitting {label} model...")
+                    model.fit(X=X_train, y=y_train, y_var=y_var_train)
+                    y_pred = model.predict()
+
+                    f_train_pred = y_pred.loc[X_train, "f"].values
+                    f_test_pred = y_pred.loc[X_test, "f"].values
+
+                    record = evaluate_predictions(
+                        y_pred, X_train, X_test, y_train, f_test, label=label, p=p
+                    )
+                    results.append(record)
+
+        results = pd.DataFrame(results)
+        results.to_csv(f"results/simulations.{base_model}.r2.csv")
--- code/simulations/fit.py
+++ code/simulations/fit.py
@@ -1,38 +1,120 @@
 import numpy as np
 import pandas as pd
-from gpmap.inference import LocalEpistasisRegression
+from gpmap.inference import LocalEpistasisRegression, SitesVCregression
 
 if __name__ == "__main__":
-    print('Loading data...')
-    data = pd.read_csv("data/processed/simulations.train.csv", index_col=0)
-    X, y, y_var = (data.index.values, data["y"].values, data["y_var"].values)
-    print(f'  Loaded {X.shape[0]} training sequences')
-    
-    test = pd.read_csv("data/processed/simulations.test.csv", index_col=0)
-    X_test = test.index.values
-    print(f'  Loaded {X_test.shape[0]} test sequences')
+    for model_name in ["ssVC", "ler"]:
+        print(f"Loading data simulated under {model_name}...")
+        data = pd.read_csv(
+            f"data/processed/simulations.{model_name}.train.csv", index_col=0
+        )
+        X, y, y_var = (
+            data.index.values,
+            data["y"].values,
+            data["y_var"].values,
+        )
+        print(f"  Loaded {X.shape[0]} training sequences")
 
-    print("Learning interaction strenghts...")
-    model = LocalEpistasisRegression(seq_length=8, alphabet_type="dna", P=2)
-    model.fit(X, y, y_var=y_var)
+        test = pd.read_csv(
+            f"data/processed/simulations.{model_name}.test.csv", index_col=0
+        )
+        X_test = test.index.values
+        print(f"  Loaded {X_test.shape[0]} test sequences")
 
-    print("Making predictions for the test set under the inferred prior...")
-    pred = model.predict(X_test)
-    test = test.join(pred, rsuffix='pred')
-    
-    print("Saving correlations under the inferred prior...")
-    corrs_df = model.get_empirical_pred_correlations_df()
-    corrs_df['seq'] = corrs_df.index
-    corrs_df.to_csv('results/simulations.corrs.csv', index=False)
-    
-    print("Saving interaction strenghts a_ij")
-    position_labels = np.arange(1, model.seq_length + 1)
-    a_values = model.get_a_values(position_labels=position_labels)
-    a_values = pd.pivot_table(a_values, index='site1', columns='site2', values='interaction_strength')
-    a_values = a_values.reindex(position_labels).fillna(0).T.reindex(position_labels).fillna(0).T
-    a_values = (a_values + a_values.T)
-    a_values.to_csv('results/simulations.inferred_interaction_strength.csv')
-    
-    print("Saving model predictions on test set...")
-    test.to_csv('results/simulations.pred.csv')
-    print('Done.')
+        #################################################################
+
+        print("Fitting Local Epistasis Regression model...")
+        print("  Learning interaction strenghts...")
+        model = LocalEpistasisRegression(
+            seq_length=8, alphabet_type="dna", P=2
+        )
+        model.fit(X, y, y_var=y_var)
+
+        print("  Saving correlations under the inferred prior...")
+        corrs_df = model.get_empirical_pred_correlations_df()
+        corrs_df["seq"] = corrs_df.index
+        corrs_df.to_csv(
+            f"results/simulations.{model_name}.corrs.csv", index=False
+        )
+
+        print("  Saving inferred lambda_U values...")
+        lambda_U = pd.DataFrame(
+            {
+                "U": corrs_df["seq"].values,
+                "k": corrs_df["d"].values,
+                "d_jittered": corrs_df["d_jittered"].values,
+                "lambda_U": model.lambdas,
+            }
+        )
+        lambda_U.to_csv(
+            f"results/simulations.{model_name}.inferred_lambda_U.ler.csv",
+            index=False,
+        )
+
+        print("  Saving interaction strenghts a_ij...")
+        position_labels = np.arange(1, model.seq_length + 1)
+        a_values = model.get_a_values(position_labels=position_labels)
+        a_values = pd.pivot_table(
+            a_values,
+            index="site1",
+            columns="site2",
+            values="interaction_strength",
+        )
+        a_values = (
+            a_values.reindex(position_labels)
+            .fillna(0)
+            .T.reindex(position_labels)
+            .fillna(0)
+            .T
+        )
+        a_values = a_values + a_values.T
+        a_values.to_csv(
+            f"results/simulations.{model_name}.inferred_interaction_strength.csv"
+        )
+
+        print(
+            "  Making predictions for complete landscapes under the inferred prior..."
+        )
+        pred = model.predict()
+        pred.to_csv(
+            f"results/simulations.{model_name}.pred.ler.csv", index=False
+        )
+
+        print("  Making predictions for the test set under the inferred prior...")
+        np.random.seed(0)
+        X_test = np.random.choice(X_test, size=200, replace=False)
+        pred = model.predict(X_test, calc_variance=True)
+        test = test.join(pred, rsuffix='pred')
+        test.to_csv(f'results/simulations.{model_name}.pred.ler.csv')
+
+        #################################################################
+
+        print(
+            "Fitting sites-structured Variance Component Regression model..."
+        )
+        print("  Learning interaction strenghts...")
+        model = SitesVCregression(seq_length=8, alphabet_type="dna")
+        model.fit(X, y, y_var=y_var)
+
+        print("  Saving inferred lambda_U values...")
+        model.vc_df.to_csv(
+            f"results/simulations.{model_name}.inferred_lambda_U.ssVC.csv",
+            index=False,
+        )
+
+        print(
+            "  Making predictions for complete landscapes under the inferred prior..."
+        )
+        pred = model.predict()
+        pred.to_csv(
+            f"results/simulations.{model_name}.pred.ssVC.csv", index=False
+        )
+
+        print("  Making predictions for the test set under the inferred prior...")
+        np.random.seed(0)
+        X_test = np.random.choice(X_test, size=200, replace=False)
+        pred = model.predict(X_test, calc_variance=True)
+        test = test.join(pred, rsuffix='pred')
+        test.to_csv(f'results/simulations.{model_name}.pred.ssVC.csv')
+
+    print("Done.")
